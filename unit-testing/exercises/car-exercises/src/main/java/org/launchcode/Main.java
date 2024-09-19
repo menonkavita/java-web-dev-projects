8581de72cb2f151dd5eb3fe3203e72f2e2d591bb
K M <menon_kavita@yahoo.com>
2024-09-18 22:55:00 -0500

    Chapter 6: Unit Testing Exercises

--- unit-testing/exercises/car-exercises/src/main/java/org/launchcode/Main.java
+++ unit-testing/exercises/car-exercises/src/main/java/org/launchcode/Main.java
@@ -4,5 +4,23 @@ public class Main {
     public static void main(String[] args) {
         Car car = new Car("Toyota", "Prius", 10, 50);
         System.out.println(car.getMake() + " - " + car.getModel());
+
+        carStats(car);
+
+        car.drive(501);
+        carStats(car);
+        car.addGas(7);
+        carStats(car);
+        car.drive(250);
+        carStats(car);
+
+        car.addGas(7.5);
+        carStats(car);
+    }
+
+    public static void carStats(Car carObj){
+        System.out.println("\nGas in Tank:                      " + carObj.getGasTankLevel()  + "\n" +
+                "Total Gas that can be in the Tank: " + carObj.getGasTankSize()  + "\n" +
+                "Odometer (Number of miles covered): " + carObj.getOdometer() + "\n");
     }
 }
